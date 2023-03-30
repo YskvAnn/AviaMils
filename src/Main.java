4c97cc5cefb3651db11da4c8ab1d95ee8a4325fb
AnnNetoTest <yaskovaann2002@mail.ru>
2023-03-30 14:26:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 13_000;
+        int oneMile = 20;
+        String mile = ("миль начислено за купленный билет ");
+        System.out.println(ticket / 20 + " миль начислено за купленный билет ");
+    }
+}
+
