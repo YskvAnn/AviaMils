c7412ea42b70d2cc4f2bc3bb2d5f2738b1c5aa63
AnnNetoTest <yaskovaann2002@mail.ru>
2023-04-04 11:34:07 +0300

    chengedCommit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 13_000;
-        int oneMile = 20;
-        String mile = ("миль начислено за купленный билет ");
-        System.out.println(ticket / 20 + " миль начислено за купленный билет ");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
 
